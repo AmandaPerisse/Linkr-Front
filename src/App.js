89f015ff4424fdd291d3a851ea8b9a0563abc7af
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-03-22 16:24:41 -0300

    feat: sign-up and login initial structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import UserContext from "./Providers/UserContext";
 
 function App() {
 
   return (
-    
+
     <BrowserRouter>
       <UserContext.Provider>
         <Routes>
-            <Route path="/" element={<Login/>}></Route>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/sign-up" element={<SignUpPage />}></Route>
         </Routes>
       </UserContext.Provider>
     </BrowserRouter>
