695b11f1f7b0c56190f7adac361423e8801ca589
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-03-26 15:58:48 -0300

    fix: BASE_URL to deployed back URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 //const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-//const BASE_URL = "https://top-linkr.herokuapp.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://top-linkr.herokuapp.com";
+//const BASE_URL = "http://localhost:5000";
 
 function createConfig(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
